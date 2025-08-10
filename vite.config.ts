42fd2261993e163d3dfa918e55634d4cde765a08
z-dn <2748311827@qq.com>
2025-08-10 13:47:41 +0800

    创建首次提交

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: './',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  }
+
+})
