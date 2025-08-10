918130b142959c5cdcbaa4aaa19e25b4f2a875a7
z-dn <2748311827@qq.com>
2025-08-10 15:17:54 +0800

    更新配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/zdn-show/master/',
+  build: {
+    outDir: 'dist'
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
